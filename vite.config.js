04653ef9387a1628fe551d57513767e137daf26f
ARIHARAN-KC <ariharan1718@gmail.com>
2025-07-02 00:01:31 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '../', // ⚠️ This makes asset paths relative for Vercel!
+  base: './', // ✅ CORRECT
 })
